2def391b70589d38177c17d95f4ea6dbac129498
UdohLawrence <lawrenceaudoh@gmail.com>
2024-05-03 08:42:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+      <div class="cards-container">
+        <div class="cards">
+          <p class="main-text"></p>
+          <p class="date"></p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main{
+  height: 100vh;
+  width: 100vw;
+}
+.container{
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+}
+header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+h1{
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
+button{
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: rgb(21,20,20);
+  border-radius: 100%;
+  color: aliceblue;
+  font-size: 20px;
+}
+</style>
